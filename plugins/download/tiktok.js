8ec56a0bc7797ce47ed665ca9a7b56d906c305ea
Ayodya Vichakshana <o.g.ayodyavichakshana@gmail.com>
2024-05-30 12:49:21 +0530

    Update tiktok.js

--- plugins/download/tiktok.js
+++ plugins/download/tiktok.js
@@ -7,6 +7,7 @@ exports.run = {
       client,
       args,
       isPrefix,
+      Scraper,
       command,
       Func
    }) => {
@@ -15,22 +16,23 @@ exports.run = {
          if (!args[0].match('tiktok.com')) return client.reply(m.chat, global.status.invalid, m)
          client.sendReact(m.chat, '🕒', m.key)
          let old = new Date()
-         const json = await Api.neoxr('/tiktok', {
-            url: Func.ttFixed(args[0])
-         })
-         if (!json.status) return m.reply(Func.jsonFormat(json))
+         //const json = await Api.neoxr('/tiktok', {
+         //   url: Func.ttFixed(args[0])
+         //})
+         const json = await Scraper.tiktok(Func.ttFixed(args[0]))
+         if (!json) return m.reply('Terjadi kesalahan')
          if (command == 'tiktok' || command == 'tt') {
-            if (json.data.video) return client.sendFile(m.chat, json.data.video, 'video.mp4', `🍟 *Fetching* : ${((new Date - old) * 1)} ms`, m)
-            if (json.data.photo) {
-               for (let p of json.data.photo) {
+            if (!json.image) return client.sendFile(m.chat, json.hdplay, 'video.mp4', `🍟 *Fetching* : ${((new Date - old) * 1)} ms`, m)
+            if (json.image) {
+               for (let p of json.image) {
                   client.sendFile(m.chat, p, 'image.jpg', `🍟 *Fetching* : ${((new Date - old) * 1)} ms`, m)
                   await Func.delay(1500)
                }
             }
          }
-         if (command == 'tikwm') return client.sendFile(m.chat, json.data.videoWM, 'video.mp4', `🍟 *Fetching* : ${((new Date - old) * 1)} ms`, m)
-         if (command == 'tikmp3') return !json.data.audio ? client.reply(m.chat, global.status.fail, m) : client.sendFile(m.chat, json.data.audio, 'audio.mp3', '', m)
-      } catch (e) {
+         if (command == 'tikwm') return !json.music ? client.reply(m.chat, global.status.fail, m) : client.sendFile(m.chat, json.wmplay, 'video.mp4', `🍟 *Fetching* : ${((new Date - old) * 1)} ms`, m)
+         if (command == 'tikmp3') return !json.music ? client.reply(m.chat, global.status.fail, m) : client.sendFile(m.chat, json.music, 'audio.mp3', '', m)
+         } catch (e) {
          return client.reply(m.chat, Func.jsonFormat(e), m)
       }
    },
